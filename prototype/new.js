4662733b897c025be55b9f0352a5ce3cdd0d828e
kanchan kahar <kaharkanchan884@gmail.com>
2025-07-07 22:10:09 +0530

    Update new.js

--- prototype/new.js
+++ prototype/new.js
@@ -6,17 +6,3 @@ console.log(multipleof2.power)  //undefined
 console.log(multipleof2.prototype)   //{}
 
 
-function createuser(username,score){
-    this.username= username;
-    this.score = score;  //for the current situation
-}
-createuser.prototype.increment = function(){
-    this.score++;
-}
-createuser.prototype.printme = function(){
-    console.log(`i love you ${this.username} ${this.score} time`)
-}
-const love = new createuser("sachin",99);
-const me = createuser("kanchan",99)
-// love.increment()
-love.printme()
