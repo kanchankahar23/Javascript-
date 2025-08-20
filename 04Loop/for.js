7d454072cfb6b6e17096718006e79e5e4f80c381
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-20 08:12:48 +0530

    loop

---
+++ 04Loop/for.js
@@ -0,0 +1,8 @@
+// for
+// 1 - 100
+// for(start ; end ; change){
+
+// }
+for(let i = 1; i< 101; i++){
+    console.log(i)
+}
---
+++ 04Loop/while.js
@@ -0,0 +1,16 @@
+//while loop 
+
+
+start
+while(end ){
+//   code 
+change
+
+}
+
+// 1-32
+let i = 1;
+while( i <32){
+    console.log(i);
+    i++;
+}
