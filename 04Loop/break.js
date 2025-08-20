125eb7b212f890fc502d52599119bc6796ebcb16
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-20 09:35:32 +0530

    solve the question

--- 04Loop/break.js
+++ 04Loop/break.js
@@ -16,3 +16,25 @@ for(let i =1 ; i<201 ; i++){
     }
     console.log(i);
 }
+
+
+
+// break and continue question 
+// Q1 print first 5 odd number only 
+// write a loop from 1 to 100 that 
+//  prints only 5 odd number
+//  then stops the loop 
+
+// use both if continue and counter + break 
+// expected output: 1 3 5 7 9
+
+let count = 0;
+for(let h =1; h<=100; h++){
+    if(h % 2 === 1){
+        count++;
+        console.log(h);
+    }
+    if(count === 5 ){
+        break
+    }
+}
