91f0c61e3549af390713ef63e73a0dc65e23eb38
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-20 23:01:20 +0530

    function

--- 05Function/script.js
+++ 05Function/script.js
@@ -1,1 +1,37 @@
-// function = 
+// function =
+
+// function declarations(statement)
+function abcd(){
+
+}
+
+// function expression
+let fnc = function(){
+
+}
+
+// Arrow function
+let fn = () =>{
+ console.log("arrow function");
+}
+
+fn();
+
+// function with parameter and arguments
+function add(v1,v2){
+    console.log(v1+v2);
+}
+add(2,3);  //argument
+
+// default
+function add(v1,v2){
+    console.log(v1 ,v2);
+}
+
+add();  //undefined
+
+function add(v1,v2){
+    console.log(v1+v2);   //NaN
+}
+
+add(); 
