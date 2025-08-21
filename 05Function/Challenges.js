af14a8dc7f4b9a1dfe3e59d0c98c69f09252476b
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-21 09:27:03 +0530

    change

--- 05Function/Challenges.js
+++ 05Function/Challenges.js
@@ -1,5 +1,44 @@
 // use rest parameter to accept any number of scores and return the total
 function getscore(...scores){
 console.log(scores);
+let total = 0;
+scores.forEach(function(val){
+    total= total + val;
+})
+return total;
 }
-getscore(10,20,30,40,50)
+console.log(getscore(10,20,30,40,50));
+
+
+///////////////////
+function checkAge(age){
+    if(age < 18 ) return "too young";
+    return "allowed";
+}
+
+console.log(checkAge(24));
+
+console.log(checkAge(14));
+
+// pass  a function into another function and execute it inside
+function abcd(val){
+    val();
+}
+
+abcd(function(){
+    console.log("hey");
+}
+);
+
+// closures
+function outer(){
+    let count = 0;
+    return function(){
+        count++;
+        console.log(count);
+    };
+}
+
+const counter = outer();
+counter();
+counter();
---
+++ 05Function/Practice.js
@@ -0,0 +1,11 @@
+// write  a BMI calculator
+ function BMI (weight, height){
+    return weight / (height * height);
+
+ }
+ console.log(BMI(56,1.5).toFixed(2));
+
+
+//  craete a reusable discount calculator (hof)
+
+function discountcalcul
