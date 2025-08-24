c879cfe95466a9c686d837a2d363b411f43085c4
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-24 22:23:42 +0530

    modify

--- 09Eventhandling'/formvalid.js
+++ 09Eventhandling'/formvalid.js
@@ -1,5 +1,14 @@
 let form = document.querySelector("form");
-form.addEventListener("submit",function(dets){
-dets.preventDefault();
+let inp = document.querySelectorAll("input");
+form.addEventListener("submit", function (dets) {
+    dets.preventDefault();
 
+    // console.log(inp);
+    // console.log(inp[0].value);
+    //     console.log(inp[1].value)
+    //     console.log(inp[2].value)
+    //     console.log(inp[3].value)
+    //     console.log(inp[4].value)    
+
+    
 })
