478a6e92ec7ff786a535dc4fdee96279dcd93c22
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-24 09:39:12 +0530

    add event.html and event.js for initial event handling setup

---
+++ 09Eventhandling'/event.js
@@ -0,0 +1,12 @@
+// event listener ka mtlb hai ki action ka reaction ho 
+let h1 =document.querySelector("h1");
+h1.addEventListener("click",()=>{
+    h1.style.color = "red";
+})
+
+let p = document.querySelector("p");
+p.addEventListener("dblclick",()=>{
+    p.style.color = "green";
+})
+
+// 
