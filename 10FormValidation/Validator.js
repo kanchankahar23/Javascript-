727d9159975a5df6f5ce5dad0af981e35c9d0c1f
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-26 00:19:07 +0530

    new

--- 10FormValidation/Validator.js
+++ 10FormValidation/Validator.js
@@ -4,17 +4,30 @@ let password = document.querySelector("#password");
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const emailregex = " ^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}";
-    const passwordregex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}";
-
+    const emailregex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 
     let emailans = emailregex.test(email.value);
     let passwordans = passwordregex.test(password.value);
+
+    let isvalid = true;
     if (!emailans) {
-        // error.style.display = "initial";
-        let error = document.querySelector("#result-msg");
-        error.textContent = "YOU TYPE WRONG TEXT"
+        let error = document.querySelector("#emailError");
+
+        error.textContent = "YOU TYPE WRONG EMAIL"
+        error.style.display = "initial";
+        isvalid = false;
+    }
+    if (!passwordans) {
+
+        let errorpas = document.querySelector("#passwordError");
+        errorpas.textContent = "YOU TYPE WRONG password";
+        errorpas.style.display = "initial";
+        isvalid = false;
+    } 
+    if(isvalid){
+document.querySelector("#result-msg").textContent = "everything is okay "
     }
 })
 
