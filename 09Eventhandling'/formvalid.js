ddf43355a8e15a51ed62e37829f070dd7892ec3d
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-25 08:52:41 +0530

    add

--- 09Eventhandling'/formvalid.js
+++ 09Eventhandling'/formvalid.js
@@ -1,14 +1,35 @@
 let form = document.querySelector("form");
+let formcontainer = document.querySelector("#forms");
 let inp = document.querySelectorAll("input");
 form.addEventListener("submit", function (dets) {
     dets.preventDefault();
+    let card = document.createElement("div");
+    card.classList.add("card");
 
-    // console.log(inp);
-    // console.log(inp[0].value);
-    //     console.log(inp[1].value)
-    //     console.log(inp[2].value)
-    //     console.log(inp[3].value)
-    //     console.log(inp[4].value)    
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
 
-    
+    let img = document.createElement("img");
+    img.setAttribute("src", inp[0].value);
+
+    let h2 = document.createElement("h2");
+    h2.textContent = `${inp[1].value}`
+    let h3 = document.createElement("h3");
+    h3.textContent = `${inp[2].value}`;
+    let p = document.createElement("p");
+    p.textContent = `${inp[3].value}`;
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h2);
+    card.appendChild(h3);
+    card.appendChild(p);
+
+    formcontainer.appendChild(card);
+    inp.forEach(function (i) {
+        if (inp != submit) {
+            i.value = " ";
+        }
+
+    })
+
 })
