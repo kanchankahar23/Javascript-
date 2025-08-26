c029e70677a0718a494f34376c8907db31a76265
kanchan kahar <kaharkanchan884@gmail.com>
2025-08-26 07:07:02 +0530

    add initial HTML and JavaScript files for timer functionality

---
+++ 11TimersAndInterval/script.js
@@ -0,0 +1,14 @@
+// setTimeout
+let tm = setTimeout(function(){
+console.log("hello")
+},5000)
+
+
+// setInterval = har 5 second me chlega
+setInterval(function(){
+    console.log("setinterval")
+}, 4000);
+
+// clearTimeout
+// clearInterval
+clearTimeout(tm);
