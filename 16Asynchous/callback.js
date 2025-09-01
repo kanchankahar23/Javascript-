63b945a1ef91b4a2a0848a58c9a237d9e86f5648
kanchan kahar <kaharkanchan884@gmail.com>
2025-09-01 14:13:09 +0530

    Refactor callback functions to improve readability and enable logging of fetched data

--- 16Asynchous/callback.js
+++ 16Asynchous/callback.js
@@ -1,7 +1,24 @@
 // ek function ko agr ap ek aur function bhej de rhe ho parameter me to vo function callback function kehlata h '
 function profile(username ,cb){
-    console.log("fetching profile data....");
+    // console.log("fetching profile data....");
     setTimeout(() =>{
-        cb({_id:121212,username})
-    })
+        console.log("fetching the data...")
+        cb({_id:121212,username ,age:26, email:"kanchan@gmail.com"})
+    },3000)
 }
+
+function post(id,cb){
+    // console.log("fetching the post....")
+    setTimeout(()=>{
+        console.log("fetching the post....");
+        cb({id:id,post:["kanchhan,hey,hello"]});
+    },2000);
+}
+
+
+profile("qaran",function(e){
+console.log(e);
+post(12,function(ele){
+    console.log(ele)
+})
+})
